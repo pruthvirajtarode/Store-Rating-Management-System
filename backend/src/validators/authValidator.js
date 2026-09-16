bab6dc9cb86a26abe001674e9e74ee697252b8d3
Pruth <pruth@bindu.dev>
2026-09-16 19:49:58 +0530

    fix: update name validation to allow 2-60 characters

--- backend/src/validators/authValidator.js
+++ backend/src/validators/authValidator.js
@@ -7,7 +7,7 @@ const passwordRegex = /^(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,16}$/;
 
 const signupValidation = [
   body('name')
-    .isLength({ min: 20, max: 60 }).withMessage('Name must be between 20 and 60 characters.'),
+    .isLength({ min: 2, max: 60 }).withMessage('Name must be between 2 and 60 characters.'),
   body('email')
     .isEmail().withMessage('Please enter a valid email address.')
     .normalizeEmail(),
